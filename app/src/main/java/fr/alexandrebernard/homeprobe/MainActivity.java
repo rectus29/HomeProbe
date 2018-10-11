82736e08aca2bd1d50d35ca029e9547f8780005c
rectus_29 <tenshi29GITHUB>
2018-10-11 14:44:14 +0200

    Initial commit

---
+++ app/src/main/java/fr/alexandrebernard/homeprobe/MainActivity.java
@@ -0,0 +1,26 @@
+package fr.alexandrebernard.homeprobe;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+	}
+
+	private boolean isNetworkAvailable() {
+		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
+
+
+}
